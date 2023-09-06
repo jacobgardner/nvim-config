44f7789a5b1e6fc9eea1d6d4d72503d8025e9689
Jacob V. Gardner <jacob.gardner@wonderlic.com>
2023-09-06 10:45:01 -0500

    Fix windowpicker, change spectre background color

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -167,7 +167,7 @@ bootstrap({
 			{
 				-- only needed if you want to use the commands with "_with_window_picker" suffix
 				"s1n7ax/nvim-window-picker",
-				version = "2.*",
+				version = "v1.*",
 				config = function()
 					require("window-picker").setup({
 						autoselect_one = true,
--- lua/plugins/spectre.lua
+++ lua/plugins/spectre.lua
@@ -1,8 +1,13 @@
 local spectre = require("spectre")
 
 spectre.setup({
-  open_cmd = "vertical topleft vnew",
+  -- vertical = take full height
+  -- top left = make sure we're on the far left of the screen
+  -- vnew = vertical split
+  -- +set ... change the background color to be distict from normal window
+  open_cmd = "vertical topleft vnew +set\\ winhighlight=Normal:Pmenu",
   color_devicons = true,
   live_update = true,
+  -- Avoids breaking spectre UI when using delete/backspace
   is_block_ui_break = true,
 })
